42b396d4a342922b38912bc1a43b6188e207196c
Aliagho <ali.bahrami633@yahoo.com>
2020-02-18 21:23:18 +1100

    finished practice

---
+++ practice-js-dom/script.js
@@ -0,0 +1,21 @@
+// Create your HTML Page via DOM Methods here!
+var bodyTag = document.querySelector("body");
+var h1Tag1 = document.createElement("h1");
+var h2Tag1 = document.createElement("h2");
+var divTag = document.createElement("div");
+var imgTag = document.createElement("img");
+
+bodyTag.appendChild(divTag);
+
+divTag.appendChild(h1Tag1);
+h1Tag1.innerText = "I am H1";
+h1Tag1.style.textAlign = "center";
+
+divTag.appendChild(h2Tag1);
+h2Tag1.innerText = "I am H2";
+h2Tag1.style.textAlign = "center";
+
+bodyTag.appendChild(divTag);
+divTag.appendChild(imgTag);
+imgTag.style.width = "100%";
+imgTag.style.margin = "50%";
